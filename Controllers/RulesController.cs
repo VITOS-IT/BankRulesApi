263a7bd848fcd7cf71d81d596db997cbfd23b62b
VITOS-IT <vitos.iva@gmail.com>
2021-11-07 13:36:42 +0200

    init

---
+++ Controllers/RulesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RulesMicroservice.Models;
+using RulesMicroservice.Services;
+
+namespace RulesMicroservice.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class RulesController : ControllerBase
+    {
+        private readonly RulesService _service;
+
+        public RulesController(RulesService service)
+        {
+            _service = service;
+        }
+        [Route("GetRules")]
+        [HttpGet]
+        public Task<string> GetRules(float balance, int accId)
+        {
+            var rule = _service.EvaluateMinBal(balance, accId);
+            return rule;
+        }
+        [Route("GetCharges")]
+        [HttpGet]
+        public Task<float> GetCharges(float balance, int accId)
+        {
+            var charge = _service.GetServiceCharges(balance, accId);
+            return charge;
+        }
+
+    }
+}
---
+++ Services/RulesService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RulesMicroservice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace RulesMicroservice.Services
+{
+    public class RulesService
+    {
+        private string accountApi = "https://bankaccountapi.azurewebsites.net/api/";
+       // private string accountApi = "http://localhost:41001/api/";
+        public async Task<string> EvaluateMinBal(float balance, int accountId)
+        {
+            var account = new AccountDTO();
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(accountApi);
+            var getTask = client.GetAsync("account/getAccount/" + accountId);
+            try
+            {
+                getTask.Wait();
+                var result = getTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    account = await result.Content.ReadFromJsonAsync<AccountDTO>();
+                    if (account.Balance > balance)
+                    {
+                        return "Allowed";
+                    }
+                    else
+                    {
+                        return "Denied. Not enough funds for a transaction";
+                    }
+                }
+            }
+            catch (DbUpdateConcurrencyException Dbce)
+            {
+                Console.WriteLine(Dbce.Message);
+            }
+            catch (DbUpdateException Dbe)
+            {
+                Console.WriteLine(Dbe.Message);
+            }
+            return "NA";
+        }
+
+        public async Task<float> GetServiceCharges(float balance, int accountId)
+        {
+            var account = new AccountDTO();
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(accountApi);
+            var getTask = client.GetAsync("Account/" + accountId);
+            getTask.Wait();
+            var result = getTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                account = await result.Content.ReadFromJsonAsync<AccountDTO>();
+                if (account.Balance < balance)
+                {
+                    var charge = (float)(balance * 0.05f);
+                    return charge;
+                }
+            }
+            return 0;
+        }
+    }
+}
